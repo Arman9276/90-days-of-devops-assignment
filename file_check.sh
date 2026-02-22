c961df9294d9354c0c0f393926463a8a6e8642f4
Arman Narsinh <armannarsinh08@gmail.com>
2026-02-23 00:02:29 +0530

    day-16 task done

--- file_check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Enter filename: " filename
-
-if [ -f "$filename" ]; then
-    echo "File exists"
-else
-    echo "File does not exist"
-fi
--- greet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p "Enter name: " NAME
-
-read -p "Enter Your Favourite Tool: " TOOL
-
-echo "Hello $NAME, your favourite tool is $TOOL"
